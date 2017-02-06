a392a359ac56488acd0a31f7925b8446acc73a5a
LeftShiftJones <jonesr1@mail.gvsu.edu>
2017-02-06 10:31:54 -0500

    Added View package and created ViewController class that will render UI.

---
+++ Euchre/Euchre/src/view/ViewController.java
@@ -0,0 +1,14 @@
+package view;
+import controller.*;
+import models.*;
+
+public class ViewController {
+    
+    /**
+     * Render class renders UI for the Euchre game.
+     * Passes no parameter as of yet.
+     */
+    public static void render() {
+	
+    }
+}

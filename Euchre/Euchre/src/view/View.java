6674ed4a1776d169dcee09c5e472b4f62fcd56a1
hultinj <hultinj@HULTINJ02.bdo.com>
2017-02-10 10:00:59 -0500

    added stuff

---
+++ Euchre/Euchre/src/view/View.java
@@ -0,0 +1,19 @@
+package view;
+import javax.swing.JFrame;
+
+import controller.*;
+import models.*;
+
+public class View {
+    
+    /**
+     * Render class renders UI for the Euchre game.
+     */
+    public static void render(GameModel model) {
+	
+    	JFrame frame = new JFrame("Eurchre");
+    	frame.setSize(800, 600);
+  
+    	
+    }
+}

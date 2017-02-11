fbeaa6634e3dced3dd1d39ca712f87eba5e1d98c
Jaredt Hultink <jdhultink@gmail.com>
2017-02-11 12:51:10 -0500

    Added content. Currently it holds the values for four hands, the 3 hidden cards, and trump

--- Euchre/Euchre/src/models/Deal.java
+++ Euchre/Euchre/src/models/Deal.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.lang.reflect.Array;
+
 /**
  * This class holds the result of a deal, which is returned by the GameDeck.Deal() method
  * @author Jaredt Hultink, Ryan Jones, Keith Rodgers
@@ -7,10 +9,101 @@ package models;
  */
 public class Deal {
 
-
+	private Hand redOne;
+	private Hand redTwo;
+	private Hand blackOne;
+	private Hand blackTwo;
+	
+	Card[] hiddenCards = new Card[3];
+	
+	Card trump;
 	
 	Deal() {
-		// TODO Auto-generated constructor stub
+		
+	}
+
+	/**
+	 * @return the redOne
+	 */
+	public Hand getRedOne() {
+		return redOne;
+	}
+
+	/**
+	 * @param redOne the redOne to set
+	 */
+	public void setRedOne(Hand redOne) {
+		this.redOne = redOne;
+	}
+
+	/**
+	 * @return the redTwo
+	 */
+	public Hand getRedTwo() {
+		return redTwo;
+	}
+
+	/**
+	 * @param redTwo the redTwo to set
+	 */
+	public void setRedTwo(Hand redTwo) {
+		this.redTwo = redTwo;
+	}
+
+	/**
+	 * @return the blackOne
+	 */
+	public Hand getBlackOne() {
+		return blackOne;
+	}
+
+	/**
+	 * @param blackOne the blackOne to set
+	 */
+	public void setBlackOne(Hand blackOne) {
+		this.blackOne = blackOne;
+	}
+
+	/**
+	 * @return the blackTwo
+	 */
+	public Hand getBlackTwo() {
+		return blackTwo;
+	}
+
+	/**
+	 * @param blackTwo the blackTwo to set
+	 */
+	public void setBlackTwo(Hand blackTwo) {
+		this.blackTwo = blackTwo;
+	}
+
+	/**
+	 * @return the hiddenCards
+	 */
+	public Card[] getHiddenCards() {
+		return hiddenCards;
+	}
+
+	/**
+	 * @param hiddenCards the hiddenCards to set
+	 */
+	public void setHiddenCards(Card[] hiddenCards) {
+		this.hiddenCards = hiddenCards;
+	}
+
+	/**
+	 * @return the trump
+	 */
+	public Card getTrump() {
+		return trump;
+	}
+
+	/**
+	 * @param trump the trump to set
+	 */
+	public void setTrump(Card trump) {
+		this.trump = trump;
 	}
 	
 }

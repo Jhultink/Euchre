ff1c0e1fc1d6b825b45cb484819f4aef51abb400
Jaredt Hultink <jdhultink@gmail.com>
2017-02-11 12:53:08 -0500

    minor changes

--- Euchre/Euchre/src/controller/TestUI.java
+++ Euchre/Euchre/src/controller/TestUI.java
@@ -2,6 +2,7 @@ package controller;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import models.GameModel;
@@ -11,12 +12,19 @@ import view.View;
 
 public class TestUI {
 
+	View view;
+	
+	@Before
+	public void setupView(){
+		view = new View();
+	}
+	
 	@Test
 	public void test() {
 		GameModel model = new GameModel();		
 		model.newHand(Teams.RED, PlayerNumber.FIRST);
 		
-		View.render(model);
-	}
+		view.render(model);
 
+	}
 }

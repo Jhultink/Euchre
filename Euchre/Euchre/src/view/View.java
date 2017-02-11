c12a2807d9654e89418dadc43c84cb7e4eb0bf72
LeftShiftJones <jonesr1@mail.gvsu.edu>
2017-02-11 16:17:32 -0500

    Updated View Class
    -Changed ActionListener to MouseListener
    -Changed method of adding buttons to implement MouseListeners
    -Added Mouse Listener methods*
    	*Only one method (mouseClicked) executes, prints text of button and
    disables the pressed button.
    -Commented out panel revalidations and repaints*
    	*Since each panel was added to the frame, only a frame revalidate and
    repaint is required.

--- Euchre/Euchre/src/view/View.java
+++ Euchre/Euchre/src/view/View.java
@@ -4,6 +4,8 @@ import controller.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.*;
 import java.util.*;
@@ -11,7 +13,7 @@ import java.util.*;
 import controller.*;
 import models.*;
 
-public class View extends JFrame implements ActionListener {
+public class View extends JFrame implements MouseListener {
 
     private JFrame frame;
     private JMenuBar menu;
@@ -74,53 +76,125 @@ public class View extends JFrame implements ActionListener {
 	topPanel.removeAll();
 	topPanel.add(Box.createHorizontalGlue()); // for spacing
 	for (Card card : model.getHandOf(Teams.RED, PlayerNumber.FIRST).getCards()) {
-	    topPanel.add(new JButton(card.getCardStringValue()));
-	    new JButton(card.getCardStringValue()).addActionListener(this);
+	    JButton button = new JButton(card.getCardStringValue());
+	    button.addMouseListener(this);
+	    topPanel.add(button);
 	}
 	topPanel.add(Box.createHorizontalGlue()); // for spacing
-	topPanel.revalidate();
-	topPanel.repaint();
+	// topPanel.revalidate();
+	// topPanel.repaint();
 
 	// Clear panel and add cards
 	rightPanel.removeAll();
 	rightPanel.add(Box.createVerticalGlue()); // for spacing
 	for (Card card : model.getHandOf(Teams.BLACK, PlayerNumber.SECOND).getCards()) {
-	    rightPanel.add(new JButton(card.getCardStringValue()));
+	    JButton button = new JButton(card.getCardStringValue());
+	    button.addMouseListener(this);
+	    rightPanel.add(button);
 	}
 	rightPanel.add(Box.createVerticalGlue()); // for spacing
-	rightPanel.revalidate();
-	rightPanel.repaint();
+	// rightPanel.revalidate();
+	// rightPanel.repaint();
 
 	// Clear panel and add cards
 	bottomPanel.removeAll();
 	bottomPanel.add(Box.createHorizontalGlue()); // for spacing
 	for (Card card : model.getHandOf(Teams.RED, PlayerNumber.SECOND).getCards()) {
-	    bottomPanel.add(new JButton(card.getCardStringValue()));
+	    JButton button = new JButton(card.getCardStringValue());
+	    button.addMouseListener(this);
+	    bottomPanel.add(button);
 	}
 	bottomPanel.add(Box.createHorizontalGlue()); // for spacing
-	bottomPanel.revalidate();
-	bottomPanel.repaint();
+	// bottomPanel.revalidate();
+	// bottomPanel.repaint();
 
 	// Clear panel and add cards
 	leftPanel.removeAll();
 	leftPanel.add(Box.createVerticalGlue()); // for spacing
 	for (Card card : model.getHandOf(Teams.BLACK, PlayerNumber.FIRST).getCards()) {
-	    leftPanel.add(new JButton(card.getCardStringValue()));
+	    JButton button = new JButton(card.getCardStringValue());
+	    button.addMouseListener(this);
+	    leftPanel.add(button);
 	}
 	leftPanel.add(Box.createVerticalGlue()); // for spacing
-	leftPanel.revalidate();
-	leftPanel.repaint();
+	// leftPanel.revalidate();
+	// leftPanel.repaint();
 
 	frame.revalidate();
 	frame.repaint();
 
     }
-    
+
     /**
      * Responds to click functions.
-     * @param ae System registered event
+     * 
+     * @param ae
+     *            System registered event
      */
     public void actionPerformed(ActionEvent ae) {
-	
+
+    }
+
+    /**
+     * Method responds to Mouse Click Currently only displays text of button
+     * clicked.
+     * 
+     * @param event
+     *            MouseEvent registered
+     */
+    public void mouseClicked(MouseEvent event) {
+	Object obj = event.getSource();
+	JButton testButton = null;
+	String buttonText = "";
+
+	if (obj instanceof JButton) {
+	    testButton = (JButton) obj;
+	}
+
+	if (testButton != null) {
+	    buttonText = testButton.getText();
+	    JOptionPane.showMessageDialog(frame, buttonText);
+	    testButton.setEnabled(false);
+	}
+    }
+
+    /**
+     * Mouse Event to run when mouse event is exited.
+     * 
+     * @param event
+     *            Registered Mouse Event
+     */
+    public void mouseEntered(MouseEvent event) {
+
+    }
+
+    /**
+     * Mouse Event to run when mouse event is exited.
+     * 
+     * @param event
+     *            Registered Mouse Event
+     */
+    public void mouseExited(MouseEvent event) {
+
+    }
+
+    /**
+     * Mouse Event to run when mouse event is exited.
+     * 
+     * @param event
+     *            Registered Mouse Event
+     */
+    public void mouseReleased(MouseEvent event) {
+
+    }
+
+    /**
+     * Mouse Event to run when mouse event is exited.
+     * 
+     * @param event
+     *            Registered Mouse Event
+     */
+    public void mousePressed(MouseEvent event) {
+
     }
 }

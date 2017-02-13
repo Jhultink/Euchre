9d7e0f79d55f83460b259f3f19af607da61c635a
Jaredt Hultink <jdhultink@gmail.com>
2017-02-12 22:00:12 -0500

    Added package info

---
+++ Euchre/Euchre/src/controller/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author Jaredt Hultink, Keith Rodgers, Ryan Jones
+ * This package hold all the controller classes for the MVC framework
+ */
+package controller;
---
+++ Euchre/Euchre/src/view/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author Jaredt Hultink, Keith Rodgers, Ryan Jones
+ * This package contains all the view classes of the MVC framework.
+ */
+package view;
